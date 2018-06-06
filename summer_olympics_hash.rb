aa80507d578c9476a7fced2e9d948e1ded63b504
Dayarelis Quinones <dayarelis.quinones.17@tfacorps.org>
2018-06-06 16:13:53 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -2,10 +2,10 @@
 def create_olympics_hash
   # Implement this method so that it returns a hash with the data provided on README.md
   olympics_hash = {
-    Sydney => "2000",
-    Athens => "2004",
-    Beijing => "2008",
-    London => "2012"
+    :Sydney => "2000",
+    :Athens => "2004",
+    :Beijing => "2008",
+    :London => "2012"
   }
   return olympics_hash
 end
@@ -21,7 +21,7 @@ end
 #   olympics_hash[:Atlanta] = "1996"
 #   return olympics_hash
 # end
-# 
+#
 # def iterate_through_hash
 #   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
 #   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
@@ -32,12 +32,12 @@ end
 #     :London => "2012"
 #   }
 #   olympics_hash[:Atlanta] = "1996"
-# 
+#
 #   olympics_hash.each do |place, year|
 #     puts "The #{place} summer olympics took place in #{year}."
 #   end
 # end
-# 
+#
 # def iterate_through_keys
 #   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
 #   # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
